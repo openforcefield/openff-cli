7c8425b86ab75a55b0ee2e15e198762716e4d4b3
Matthew W. Thompson <mattwthompson@protonmail.com>
2020-10-30 15:57:24 -0500

    Add core modules

---
+++ openff/cli/core.py
@@ -0,0 +1,99 @@
+from typing import Tuple
+
+import numpy as np
+from openforcefield.typing.engines.smirnoff import ForceField
+from openforcefield.utils.toolkits import ToolkitRegistry
+from simtk import openmm, unit
+
+
+def make_registry(toolkit: str) -> Tuple[ToolkitRegistry, str]:
+    if toolkit.lower() == "openeye":
+        import openeye
+        from openforcefield.utils.toolkits import OpenEyeToolkitWrapper
+
+        toolkit_registry = ToolkitRegistry(toolkit_precedence=[OpenEyeToolkitWrapper])
+        toolkit_version = "openeye-toolkits version " + openeye.__version__
+    elif toolkit.lower() == "rdkit":
+        import rdkit
+        from openforcefield.utils.toolkits import RDKitToolkitWrapper
+
+        toolkit_registry = ToolkitRegistry(toolkit_precedence=[RDKitToolkitWrapper])
+        toolkit_version = "rdkit version " + rdkit.__version__
+    else:
+        from openff.cli.utils.exceptions import UnsupportedToolkitError
+
+        raise UnsupportedToolkitError(toolkit=toolkit)
+
+    return toolkit_registry, toolkit_version
+
+
+def _get_forcefield(forcefield: str, constrained: bool) -> ForceField:
+    ff_name = forcefield
+    if constrained:
+        split_name = ff_name.split("-")
+        split_name[0] = "openff_unconstrained"
+        ff_name = "-".join(split_name)
+    if not ff_name.endswith(".offxml"):
+        ff_name += ".offxml"
+    ff = ForceField(ff_name)
+
+    return ff
+
+
+def _build_simulation(molecule, forcefield, mols_with_charge=None):
+    """Given a Molecule and ForceField, initialize a barebones OpenMM Simulation."""
+    off_top = molecule.to_topology()
+    system, ret_top = forcefield.create_openmm_system(
+        off_top,
+        charge_from_molecules=mols_with_charge,
+        allow_nonintegral_charges=True,
+        return_topology=True,
+    )
+
+    # Use OpenMM to compute initial and minimized energy for all conformers
+    integrator = openmm.VerletIntegrator(1 * unit.femtoseconds)
+    platform = openmm.Platform.getPlatformByName("Reference")
+    omm_top = off_top.to_openmm()
+    simulation = openmm.app.Simulation(omm_top, system, integrator, platform)
+
+    charges_from_top = [*ret_top.reference_molecules][0].partial_charges
+    if charges_from_top is not None:
+        partial_charges = charges_from_top
+    else:
+        # ret_top only has partial charges in OFFTK 0.8.0+, so may need to
+        # manually get partial charges from OpenMM if using OFFTK <= 0.7.1
+        partial_charges = [
+            system.getForces()[0].getParticleParameters(i)[0]
+            for i in range(molecule.n_atoms)
+        ]
+        # Unwrap list of Quantity objects into a single Quantity that contains a list
+        # Surely there's a simpler way to to this?
+        partial_charges = unit.Quantity(
+            np.asarray(
+                [val.value_in_unit(unit.elementary_charge) for val in partial_charges]
+            ),
+            unit=unit.elementary_charge,
+        )
+
+    return simulation, partial_charges
+
+
+def _get_conformer_data(
+    simulation: openmm.app.Simulation,
+) -> Tuple[unit.Quantity, unit.Quantity]:
+    """Given an OpenMM simulation, return its energy and coordinates"""
+    state = simulation.context.getState(getEnergy=True, getPositions=True)
+    energy = state.getPotentialEnergy()
+    coords = state.getPositions()
+
+    return energy, coords
+
+
+def _minimize_conformer(
+    simulation: openmm.app.Simulation, conformer: unit.Quantity
+) -> openmm.app.Simulation:
+    """Minimize a conformer via OpenMM"""
+    simulation.context.setPositions(conformer)
+    simulation.minimizeEnergy()
+
+    return simulation
